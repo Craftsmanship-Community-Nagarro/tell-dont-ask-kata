b1446e4c52106899ea3951be97dcdebe624aae03
Dejan Linkic <dejan.linkic@nagarro.com>
2021-03-25 17:49:54 +0100

    another one

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -10,68 +10,92 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Order {
-    private BigDecimal total;
-    private String currency;
-    private List<OrderItem> items;
-    private BigDecimal tax;
-    private OrderStatus status;
-    private int id;
+	private BigDecimal total;
+	private String currency;
+	private List<OrderItem> items;
+	private BigDecimal tax;
+	private OrderStatus status;
+	private int id;
 
-    public BigDecimal getTotal() {
-        return total;
-    }
+	public BigDecimal getTotal() {
+		return total;
+	}
 
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
 
-    public String getCurrency() {
-        return currency;
-    }
+	public String getCurrency() {
+		return currency;
+	}
 
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
 
-    public List<OrderItem> getItems() {
-        return items;
-    }
+	public List<OrderItem> getItems() {
+		return items;
+	}
 
-    public void setItems(List<OrderItem> items) {
-        this.items = items;
-    }
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
 
-    public BigDecimal getTax() {
-        return tax;
-    }
+	public BigDecimal getTax() {
+		return tax;
+	}
 
-    public void setTax(BigDecimal tax) {
-        this.tax = tax;
-    }
+	public void setTax(BigDecimal tax) {
+		this.tax = tax;
+	}
 
-    public OrderStatus getStatus() {
-        return status;
-    }
+	public OrderStatus getStatus() {
+		return status;
+	}
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-  public void verifyShippable() {
-      if (getStatus().equals(CREATED) || getStatus().equals(REJECTED)) {
-          throw new OrderCannotBeShippedException();
-      }
+	public void verifyShippable() {
+		if (getStatus().equals(CREATED) || getStatus().equals(REJECTED)) {
+			throw new OrderCannotBeShippedException();
+		}
 
-      if (getStatus().equals(SHIPPED)) {
-          throw new OrderCannotBeShippedTwiceException();
-      }
-  }
+		if (getStatus().equals(SHIPPED)) {
+			throw new OrderCannotBeShippedTwiceException();
+		}
+	}
+
+	public void approve() {
+		verifyCanBeChanged();
+		if (getStatus().equals(REJECTED)) {
+			throw new RejectedOrderCannotBeApprovedException();
+		}
+
+		setStatus(OrderStatus.APPROVED);
+	}
+
+	public void reject() {
+		verifyCanBeChanged();
+		if (getStatus().equals(OrderStatus.APPROVED)) {
+			throw new ApprovedOrderCannotBeRejectedException();
+		}
+
+		setStatus(OrderStatus.REJECTED);
+	}
+
+	private void verifyCanBeChanged() {
+		if (getStatus().equals(SHIPPED)) {
+			throw new ShippedOrdersCannotBeChangedException();
+		}
+	}
 }
--- java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderApprovalUseCase.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderApprovalUseCase.java
@@ -1,11 +1,7 @@
 package it.gabrieletondi.telldontaskkata.useCase;
 
 import it.gabrieletondi.telldontaskkata.domain.Order;
-import it.gabrieletondi.telldontaskkata.domain.OrderStatus;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
-
-import static it.gabrieletondi.telldontaskkata.domain.OrderStatus.REJECTED;
-import static it.gabrieletondi.telldontaskkata.domain.OrderStatus.SHIPPED;
 
 public class OrderApprovalUseCase {
     private final OrderRepository orderRepository;
@@ -17,29 +13,13 @@ public class OrderApprovalUseCase {
     public void run(OrderApprovalRequest request) {
         final Order order = orderRepository.getById(request.getOrderId());
 
-        if(request.isApproved()) {
-            if (order.getStatus().equals(SHIPPED)) {
-                throw new ShippedOrdersCannotBeChangedException();
-            }
+		if (request.isApproved()) {
+			order.approve();
 
-            if (order.getStatus().equals(REJECTED)) {
-                throw new RejectedOrderCannotBeApprovedException();
-            }
+		} else {
+			order.reject();
+		}
 
-            order.setStatus(OrderStatus.APPROVED);
-
-        } else {
-            if (order.getStatus().equals(SHIPPED)) {
-                throw new ShippedOrdersCannotBeChangedException();
-            }
-
-            if (order.getStatus().equals(OrderStatus.APPROVED)) {
-                throw new ApprovedOrderCannotBeRejectedException();
-            }
-
-            order.setStatus(OrderStatus.REJECTED);
-        }
-
-        orderRepository.save(order);
+		orderRepository.save(order);
     }
 }
